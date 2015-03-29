376149deb560332095e6196cde47b8bed3dc3efb
j1010c <colonia23@gmail.com>
2015-03-29 19:10:55 -0400

    Update Marauders.py

--- Marauders_GUI/Marauders.py
+++ Marauders_GUI/Marauders.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python2
 # -*- coding: utf-8 -*-
+from glob import glob
 from math import sqrt, floor, ceil
 import os, time, sys, yaml, kirk
 import tkinter.constants as C
@@ -126,7 +127,7 @@ class Marauders(Frame):
 
             else:
                 # this is an action
-                btn.configure(command=lambda act=act: self.go_action(act), )
+                btn.configure(command=lambda act=act: self.display(act), )
 
             if 'color' in item:
                 btn.set_color(item['color'])
@@ -143,7 +144,7 @@ class Marauders(Frame):
 
     def get_icon(self, name):
         """
-        Loads the given icon and saves a reference
+        Loads the given icon and keeps a reference
 
         :param name: string
         :return:
@@ -162,53 +163,54 @@ class Marauders(Frame):
 
     def hide_top(self):
         """
-        hide the top page, and returns
+        hide the top page
+        :return:
         """
         self.framestack[len(self.framestack) - 1].pack_forget()
 
     def show_top(self):
         """
-        show the top page, and returns 
+        show the top page
+        :return:
         """
         self.framestack[len(self.framestack) - 1].pack(fill=BOTH, expand=1)
 
-    def capture(self):
-      
-        width = kirk.width
-        height = kirk.height
-        box_size = kirk.box_size
+    def display(self, actions):
 
-        img = Image.PhotoImage(Image.open(kirk.File))
-        w = Canvas(root, width=width, height=height)
-        w.pack()
-        w.create_image(0, 0, image=img, anchor="nw")
+        self.hide_top()
+        self.images = glob("*.gif")
+        self.cur = 0
+        # label showing the image
+        self.image = PhotoImage()
+        imagelabel = Label(self, image=self.image)
+        imagelabel.grid(row=1, column=1)
+        imagelabel.pack()
 
-        for x in range(1, width // box_size):
-            w.create_line(box_size * x, 0, box_size * x, height)
-        for y in range(1, height // box_size):
-            w.create_line(0, box_size * y, width, box_size * y)
+        # layout and show first image
+        self.grid()
+        self.cur = (self.cur + 1) % len(self.images)
+        self.image.configure(file=self.images[self.cur])
+        self.parent.update()
 
-        # function to be called when mouse is clicked
-        #def printcoords(event):
-            #outputting x and y coords to console
-            #print(event.x, event.y)
-            #piconnect(event.x // box_size, event.y // box_size)
-            #print("DONE")
 
-        #mouseclick event
-        #w.bind("<Button 1>", printcoords)
+        def coordinates(event):
+            print(event.x, event.y)
+
+        imagelabel.bind('<Button-1>', coordinates)
 
 
     def destroy_top(self):
         """
-        destroy the top page and returns
+        destroy the top page
+        :return:
         """
         self.framestack[len(self.framestack) - 1].destroy()
         self.framestack.pop()
 
     def destroy_all(self):
         """
-        destroy all pages except the first Frame and goes back to start
+        destroy all pages except the first aka. go back to start
+        :return:
         """
         while len(self.framestack) > 1:
             self.destroy_top()
@@ -220,15 +222,15 @@ class Marauders(Frame):
         :return:
         """
         # hide the menu and show a delay screen
+        self.display(actions)
         self.hide_top()
-        self.display()
         delay = Frame(self, bg="white")
         delay.pack(fill=BOTH, expand=1)
-        label = Label(delay, text="Executing...", fg="white", bg="#2d89ef", font="Sans 30")
+        label = Label(delay, text="Action...", fg="white", bg="#2d89ef", font="Sans 30")
         label.pack(fill=BOTH, expand=1)
         self.parent.update()
 
-        self.display()
+
 
         time.sleep(.25)
 
@@ -239,8 +241,8 @@ class Marauders(Frame):
 
     def go_back(self):
         """
-        destroy the current frame and reshow the one below,
-        returns
+        destroy the current frame and reshow the one below
+        :return:
         """
         self.destroy_top()
         self.show_top()
@@ -253,7 +255,7 @@ class Marauders(Frame):
 
 def main():
     root = Tk()
-    root.geometry("520x440")
+    root.geometry("320x240")
     root.wm_title('Marauders Map')
     if len(sys.argv) > 1 and sys.argv[1] == 'fs':
         root.wm_attributes('- fullscreen', True)
